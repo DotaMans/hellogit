b9416636e38684d3bbdbb7b2a001069a31d1a797
20781 <2078199832@qq.com>
2018-08-19 16:11:47 +0800

    test git change

--- src/hellogit/HelloGit.java
+++ src/hellogit/HelloGit.java
@@ -2,7 +2,7 @@ package hellogit;
 
 public class HelloGit {
 	public static void main(String[] args) {
-		System.out.println("show you");
+		System.out.println("show you change");
 		System.out.println("Hello Git abc ");
 	}
 }
